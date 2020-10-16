43686b21c62b30607bf72ceeb00b45eb14b09a69
RinchenTuladhar <rinchentul@gmail.com>
2020-10-16 22:14:51 +0100

    Set up initial navbar & home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => "home#index"
+
+  get 'home', to: 'home#index'
+  get 'navbar/index'
+
+  resources :home
 end
