10f07adff37e6c0b891554bec80a55e0a8cba334
RinchenTuladhar <rinchentul@gmail.com>
2020-10-19 21:05:02 +0100

    Added sessions & ability to log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :create]
-
   root :to => "home#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:index, :create, :edit, :destroy]
 
   get 'register', to: "users#new"
   get 'home', to: 'home#index'
   get 'navbar/index'
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
 
   resources :home
 end
