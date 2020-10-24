260b6d7b015a642164118665e8a4d7118112a844
RinchenTuladhar <rinchentul@gmail.com>
2020-10-24 18:11:01 +0100

    Styled products list page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,8 +41,6 @@ class ProductsController < ApplicationController
   # PATCH/PUT /products/1.json
   def update
     respond_to do |format|
-      @product.images.attach(params[:images])
-
       if @product.update(product_params)
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
